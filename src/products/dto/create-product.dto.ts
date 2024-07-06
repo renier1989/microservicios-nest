17e4e919fc5385d58f20b202675c682bf57a94c3
Renier Vargas <renier1989@gmail.com>
2024-07-06 07:04:54 -0500

    Usando el endpoint para agregar productos y haciendo validaciones

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,16 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    name:string;
+
+    @IsString()
+    @IsOptional()
+    description?:string;
+
+    @IsNumber()
+    @Type( ()=>Number)  // intenta hacer una transformacion a un numero
+    price:number;
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,7 +9,8 @@ export class ProductsController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+    return createProductDto;
+    // return this.productsService.create(createProductDto);
   }
 
   @Get()
